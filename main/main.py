5db8db7c7759a25cd8b583f43b805b6cf99203b9
Shashank <svivek2@slb.com>
2020-06-07 16:26:34 +0530

    indentatiob

--- main/main.py
+++ main/main.py
@@ -14,9 +14,9 @@ if __name__ == '__main__':
     input_sentences = lines[0].split(".")
     answer_sentences = lines[6].split(";")
 
-    for line_number in range(1,6):
-        para_line = process.extractOne(lines[line_number],input_sentences,scorer=fuzz.token_set_ratio)[0]
-        print(process.extractOne(para_line,answer_sentences,scorer=fuzz.token_set_ratio)[0].strip())
+    for line_number in range(1, 6):
+        para_line = process.extractOne(lines[line_number], input_sentences, scorer=fuzz.token_set_ratio)[0]
+        print(process.extractOne(para_line, answer_sentences, scorer=fuzz.token_set_ratio)[0].strip())
 
 
 
