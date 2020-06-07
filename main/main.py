a1fc2dcabf8b5cfe05309e7ed516cc3625e8086f
Shashank <svivek2@slb.com>
2020-06-07 16:09:41 +0530

    added code

---
+++ main/main.py
@@ -0,0 +1,22 @@
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+from parser.tokenizer import *
+import argparse
+
+parser = argparse.ArgumentParser(description='Extrating answers from imput file')
+parser.add_argument('filePath', type=str, help='Path of Input File')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    token = Tokenizer(args.filePath)
+    lines = token.get_parsed_data()
+
+    input_sentences = lines[0].split(".")
+    answer_sentences = lines[6].split(";")
+
+    for line_number in range(1,6):
+        para_line = process.extractOne(lines[line_number],input_sentences,scorer=fuzz.token_set_ratio)[0]
+        print(process.extractOne(para_line,answer_sentences,scorer=fuzz.token_set_ratio)[0].strip())
+
+
+
---
+++ main/parser/tokenizer.py
@@ -0,0 +1,18 @@
+
+class Tokenizer:
+
+    def __init__(self,file_path):
+        self.__file_path = file_path
+        with open(file_path) as input_file:
+            self.lines = [line.rstrip() for line in input_file]
+    #     file not found error to be handled
+
+    def get_file_path(self):
+        return self.__file_path;
+
+    def get_parsed_data(self):
+        return self.lines
+
+    # def get_
+
+
