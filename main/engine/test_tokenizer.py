9cac89bf01dc8c0ea3dbe60976bbba7af4094129
Shashank <svivek2@slb.com>
2020-06-09 00:57:00 +0530

    removed files

--- main/engine/test_tokenizer.py
+++ main/engine/test_tokenizer.py
@@ -1,10 +1,14 @@
 import unittest
-from main.engine.tokenizer import Tokenizer
+from tokenizer import Tokenizer
 
 
 class TestTokenizer(unittest.TestCase):
 
     def test_get_parsed_data(self):
-        token = Tokenizer('../../data/input.txt')
+        token = Tokenizer('../data/input.txt')
         lines = token.get_parsed_data()
         self.assertEqual(len(lines), 7)
+
+
+if __name__ == '__main__':
+    unittest.main()
